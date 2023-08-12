b1e46dc8a42680e94c99d60accd9d1c9cd5a07de
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-08-12 22:09:51 +0200

    Add simple mock response

--- main.go
+++ main.go
@@ -5,9 +5,27 @@ import (
 	"net/http"
 )
 
+type City struct {
+	name string
+	lat  float64
+	lng  float64
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World"))
-	print()
+
+	if r.Method != "GET" {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "Mehod Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	city := r.URL.Query().Get("city")
+
+	if city == "paris" {
+		var parisJson = []byte(`{"name": "Paris","lat": 48.8566, "lng":  2.3522 }`)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(parisJson)
+	}
 }
 
 func main() {
@@ -15,6 +33,6 @@ func main() {
 	mux.HandleFunc("/", home)
 
 	log.Print("Server running on :4000")
-	err := http.ListenAndServe(":400", mux)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
